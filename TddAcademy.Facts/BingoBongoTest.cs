b60375d231d0c87459906aed63e01b2ecfb501be
Loher Marco <Marco.Loher@Zund.com>
2021-03-22 14:31:12 +0100

    Marco & Christian

--- TddAcademy.Facts/BingoBongoTest.cs
+++ TddAcademy.Facts/BingoBongoTest.cs
@@ -1,7 +1,106 @@
-﻿namespace TddAcademy.Facts
- {
-     public class BingoBongoTest
-     {
-         // todo: implement BingoBongo deriving from IBingoBongo test first
-     }
- }
+﻿using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace TddAcademy.Facts
+{
+	public class BingoBongoTest
+	{
+		#region Fields
+
+		private readonly BingoBongo _testee;
+
+		#endregion
+
+		public BingoBongoTest()
+		{
+			_testee = new BingoBongo();
+		}
+
+		[Fact]
+		public void FirstShouldReturn1()
+		{
+			var result = _testee.Play();
+
+			result.First().Should().Be("1");
+		}
+
+		[Fact]
+		public void LastShouldReturnBingoBongo()
+		{
+			var result = _testee.Play();
+
+			result.Last().Should().Be("BingoBongo");
+		}
+
+		[Fact]
+		public void MultiplyOf3ReturnsBingo()
+		{
+			var result = _testee.Play();
+
+			result[2].Should().Be("Bingo");
+			result[5].Should().Be("Bingo");
+			result[8].Should().Be("Bingo");
+			result[32].Should().Be("Bingo");
+			result[98].Should().Be("Bingo");
+		}
+
+		[Fact]
+		public void MultiplyOf5ReturnsBongo()
+		{
+			var result = _testee.Play();
+
+			result[4].Should().Be("Bongo");
+			result[9].Should().Be("Bongo");
+			result[19].Should().Be("Bongo");
+			result[54].Should().Be("Bongo");
+			result[99].Should().Be("Bongo");
+		}
+
+		[Fact]
+		public void MultiplyOf7ReturnsConga()
+		{
+			var result = _testee.Play();
+
+			result[6].Should().Be("Conga");
+			result[13].Should().Be("Conga");
+		}
+
+		[Fact]
+		public void MultiplyOf3And5ReturnsBingoBongo()
+		{
+			var result = _testee.Play();
+
+			result[14].Should().Be("BingoBongo");
+			result[29].Should().Be("BingoBongo");
+			result[44].Should().Be("BingoBongo");
+			result[89].Should().Be("BingoBongo");
+		}
+
+		[Fact]
+		public void MultiplyOf3And7ReturnsBingoConga()
+		{
+			var result = _testee.Play();
+
+			result[20].Should().Be("BingoConga");
+			result[41].Should().Be("BingoConga");
+		}
+
+		[Fact]
+		public void MultiplyOf5And7ReturnsBongoConga()
+		{
+			var result = _testee.Play();
+
+			result[34].Should().Be("BongoConga");
+			result[69].Should().Be("BongoConga");
+		}
+
+		[Fact]
+		public void MultiplyOf3And5And7ReturnsBingoBongoConga()
+		{
+			var result = _testee.Play();
+
+			result[104].Should().Be("BingoBongoConga");
+		}
+	}
+}
---
+++ TddAcademyCore/BingoBongo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace TddAcademy.Facts
+{
+	public class BingoBongo : IBingoBongo
+	{
+		#region Constants
+
+		private const int c_counterMin = 1;
+		private const int c_counterMax = 120;
+
+		#endregion
+
+		#region Interface methods
+
+		public IImmutableList<string> Play()
+		{
+			var list = new List<string>();
+			for(int counter = c_counterMin; counter <= c_counterMax; counter++)
+			{
+				if(!((counter % 3 == 0) || (counter % 5 == 0) || (counter % 7 == 0)))
+				{
+					list.Add(counter.ToString());
+					continue;
+				}
+
+				var entry = "";
+				if(counter % 3 == 0)
+					entry += "Bingo";
+				if(counter % 5 == 0)
+					entry += "Bongo";
+				if(counter % 7 == 0)
+					entry += "Conga";
+
+				list.Add(entry);
+			}
+			return list.ToImmutableArray();
+		}
+
+		#endregion
+	}
+}
