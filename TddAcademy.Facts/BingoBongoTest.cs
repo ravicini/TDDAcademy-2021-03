d59051f33939f2a38cdaefb9ce969da09b68c5f2
Loher Pascal <Pascal.Loher@zund.com>
2021-03-23 06:51:24 +0100

    Joel & Pascal: Klasse BingoBongo & passende Tests implementiert

--- TddAcademy.Facts/BingoBongoTest.cs
+++ TddAcademy.Facts/BingoBongoTest.cs
@@ -1,7 +1,90 @@
-﻿namespace TddAcademy.Facts
- {
-     public class BingoBongoTest
-     {
-         // todo: implement BingoBongo deriving from IBingoBongo test first
-     }
- }
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TddAcademy.Facts
+{
+	public class BingoBongoTest
+	{
+		[Fact]
+		public void BingoBongo_ShouldNotBeBull()
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().NotBeNull();
+		}
+
+		[Fact]
+		public void BingoBongo_ShouldReturn100Items()
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().HaveCount(100);
+		}
+
+		[Fact]
+		public void BingoBongo_ShouldReturnFirstNumber()
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().StartWith("1");
+		}
+
+		[Theory]
+		[InlineData(0, "1")]
+		[InlineData(42, "43")]
+		[InlineData(85, "86")]
+		public void BingoBongo_ShouldReturnNumbers(int index, string expected)
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().HaveElementAt(index, expected);
+		}
+
+		[Theory]
+		[InlineData(2)]
+		[InlineData(32)]
+		[InlineData(80)]
+		public void BingoBongo_ShoudReturnBingo(int index)
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().HaveElementAt(index, "Bingo");
+		}
+
+		[Theory]
+		[InlineData(4)]
+		[InlineData(24)]
+		[InlineData(64)]
+		public void BingoBongo_ShoudReturnBongo(int index)
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().HaveElementAt(index, "Bongo");
+		}
+
+		[Theory]
+		[InlineData(14)]
+		[InlineData(29)]
+		[InlineData(74)]
+		public void BingoBongo_ShoudReturnBingoBongo(int index)
+		{
+			IBingoBongo testee = new BingoBongo();
+
+			var result = testee.Play();
+
+			result.Should().HaveElementAt(index, "BingoBongo");
+		}
+	}
+}
--- TddAcademyCore/IBingoBongo.cs
+++ TddAcademyCore/IBingoBongo.cs
@@ -1,9 +1,44 @@
-﻿namespace TddAcademy
-{
-    using System.Collections.Immutable;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 
-    public interface IBingoBongo
-    {
-        IImmutableList<string> Play();
-    }
+namespace TddAcademy
+{
+	public interface IBingoBongo
+	{
+		IImmutableList<string> Play();
+	}
+
+	public class BingoBongo : IBingoBongo
+	{
+		#region Fields
+
+		private readonly List<string> _items;
+
+		#endregion
+
+		public BingoBongo()
+		{
+			_items = new List<string>();
+		}
+
+		#region Interface methods
+
+		public IImmutableList<string> Play()
+		{
+			for(int i = 1; i <= 100; i++)
+			{
+				if((i % 3 == 0) && (i % 5 == 0))
+					_items.Add("BingoBongo");
+				else if(i % 3 == 0)
+					_items.Add("Bingo");
+				else if(i % 5 == 0)
+					_items.Add("Bongo");
+				else
+					_items.Add(i.ToString());
+			}
+			return _items.ToImmutableArray();
+		}
+
+		#endregion
+	}
 }
