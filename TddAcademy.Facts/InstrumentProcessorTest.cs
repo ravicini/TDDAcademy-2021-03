d26496dfc635669e1f9d454b75274a656a539551
Loher Pascal <Pascal.Loher@zund.com>
2021-03-23 06:48:30 +0100

    Joel & Pascal: Erste zwei Tests, bei dem ein Task ausgeführt und eine Exception geworfen wird, erstellt.

--- TddAcademy.Facts/InstrumentProcessorTest.cs
+++ TddAcademy.Facts/InstrumentProcessorTest.cs
@@ -1,7 +1,99 @@
-﻿namespace TddAcademy.Tests
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace TddAcademy.Tests
 {
-    public class InstrumentProcessorTest
-    {
-        // todo: implement InstrumentProcessor test first 
-    }
+	public class InstrumentProcessorTest
+	{
+		// todo: implement InstrumentProcessor test first 
+
+		[Fact]
+		public void GetTaskFromDispatcher()
+		{
+			string taskName = "Task1";
+
+			FakeTaskDispatcher fakeDispatcher = new FakeTaskDispatcher(taskName);
+			IInstrument fakeInstrument = new FakeInstrument();
+
+			IInstrumentProcessor testee = new InstrumentProcessor(fakeDispatcher, fakeInstrument);
+
+			testee.Process();
+
+			fakeDispatcher.ExecutedTask.Should().Be(taskName);
+		}
+
+		[Fact]
+		public void InstrumentSchouldThrowException()
+		{
+			string taskName = "Exception1";
+
+			FakeTaskDispatcher fakeDispatcher = new FakeTaskDispatcher(taskName);
+			IInstrument fakeInstrument = new FakeInstrument();
+
+			IInstrumentProcessor testee = new InstrumentProcessor(fakeDispatcher, fakeInstrument);
+
+			Action act = () => testee.Process();
+
+			act.Should().Throw<Exception>()
+			   .WithMessage("Exception in fakeInstrument");
+		}
+	}
+
+	internal class FakeInstrument : IInstrument
+	{
+		#region Events
+
+		public event EventHandler<TaskEventArgs> Finished;
+		public event EventHandler<TaskEventArgs> Error;
+
+		#endregion
+
+		#region Interface methods
+
+		public void Execute(string task)
+		{
+			if(task.Contains("Exception1"))
+				throw new Exception("Exception in fakeInstrument");
+			else
+				Finished.Invoke(this, new TaskEventArgs(task));
+		}
+
+		#endregion
+	}
+
+	internal class FakeTaskDispatcher : ITaskDispatcher
+	{
+		#region Fields
+
+		private readonly string _taskName;
+		private string _executedTask;
+
+		#endregion
+
+		#region Properties
+
+		public string ExecutedTask => _executedTask;
+
+		#endregion
+
+		public FakeTaskDispatcher(string taskName)
+		{
+			_taskName = taskName;
+		}
+
+		#region Interface methods
+
+		public void FinishedTask(string task)
+		{
+			_executedTask = task;
+		}
+
+		public string GetTask()
+		{
+			return _taskName;
+		}
+
+		#endregion
+	}
 }
---
+++ TddAcademyCore/InstrumentProcessor.cs
@@ -0,0 +1,35 @@
+﻿namespace TddAcademy.Tests
+{
+	public class InstrumentProcessor : IInstrumentProcessor
+	{
+		#region Fields
+
+		private ITaskDispatcher _dispatcher;
+		private IInstrument _instrument;
+
+		#endregion
+
+		public InstrumentProcessor(ITaskDispatcher dispatcher, IInstrument instrument)
+		{
+			_dispatcher = dispatcher;
+			_instrument = instrument;
+
+			_instrument.Finished += InstrumentOnFinished;
+		}
+
+		#region Interface methods
+
+		public void Process()
+		{
+			string task = _dispatcher.GetTask();
+			_instrument.Execute(task);
+		}
+
+		#endregion
+
+		private void InstrumentOnFinished(object? sender, TaskEventArgs e)
+		{
+			_dispatcher.FinishedTask(e.Task);
+		}
+	}
+}
