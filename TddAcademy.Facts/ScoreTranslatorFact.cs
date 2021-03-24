e19b117b8592fe8c3ab774b481a92029491c9891
Simmen Stefan <Stefan.Simmen@zund.com>
2021-03-24 10:17:33 +0100

    - GetSimpleResult implementiert

---
+++ TddAcademy.Facts/ScoreTranslatorFact.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace TddAcademy.Facts
+{
+	// todo: methode deuce
+	// todo: methode advantage (A/B)
+	// todo: methode game (A/B)
+	// todo: 
+
+	public class ScoreTranslatorFact
+	{
+		[Theory]
+		[InlineData(0, 0, "love:love")]
+		[InlineData(1, 0, "fifteen:love")]
+		[InlineData(2, 0, "thirty:love")]
+		[InlineData(3, 0, "forty:love")]
+		[InlineData(0, 1, "love:fifteen")]
+		[InlineData(0, 2, "love:thirty")]
+		[InlineData(0, 3, "love:forty")]
+		[InlineData(1, 1, "fifteen:fifteen")]
+		[InlineData(2, 1, "thirty:fifteen")]
+		[InlineData(3, 1, "forty:fifteen")]
+		[InlineData(1, 2, "fifteen:thirty")]
+		[InlineData(1, 3, "fifteen:forty")]
+		[InlineData(3, 2, "forty:thirty")]
+		[InlineData(2, 3, "thirty:forty")]
+		[InlineData(3, 3, "forty:forty")]
+		public void ReturnSimpleResult(int scoreA, int scoreB, string expectedScore)
+		{
+			var testee = new ScoreTranslator();
+
+			var score = testee.GetSimpleResult(scoreA, scoreB);
+
+			score.Should().Be(expectedScore);
+		}
+	}
+}
--- TddAcademyCore/ScoreTranslator.cs
+++ TddAcademyCore/ScoreTranslator.cs
@@ -1,6 +1,24 @@
-﻿namespace TddAcademy
+﻿using System.Collections.Generic;
+
+namespace TddAcademy
 {
 	public class ScoreTranslator
 	{
+		#region Fields
+
+		private readonly Dictionary<int, string> _simpleScores = new()
+			{
+				{ 0, "love" },
+				{ 1, "fifteen" },
+				{ 2, "thirty" },
+				{ 3, "forty" }
+			};
+
+		#endregion
+
+		public string GetSimpleResult(int scoreA, int scoreB)
+		{
+			return $"{_simpleScores[scoreA]}:{_simpleScores[scoreB]}";
+		}
 	}
 }
